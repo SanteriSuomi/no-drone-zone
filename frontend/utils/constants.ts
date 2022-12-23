93afb542d6b0e20c890d199989f93218ae805586
Santeri Suomi <sant.suomi@gmail.com>
2022-12-23 16:03:39 +0200

    Update constants.ts
    Update API url

--- frontend/utils/constants.ts
+++ frontend/utils/constants.ts
@@ -1,5 +1,6 @@
 const REFRESH_SPEED = 3000;
 
-const API_URL_VIOLATIONS = "http://localhost:4000/violations";
+const API_URL_VIOLATIONS =
+	"https://no-drone-zone-backend.onrender.com/violations";
 
 export { REFRESH_SPEED, API_URL_VIOLATIONS };
